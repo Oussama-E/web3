28eafddd5aef49800462498b86974379d92b3a62
Oussama-E <oussama.elbouhtain@student.vinci.be>
2023-09-16 15:15:10 +0200

    1.1-1.2 : import image

--- Exercises/module1/exo1.1-1.2/src/components/Header/Header.jsx
+++ Exercises/module1/exo1.1-1.2/src/components/Header/Header.jsx
@@ -1,8 +1,11 @@
+import logo from './stack_img.png'
+
 const Header = (props) => {
 
     console.log(props)
     return (
       <div>
+        <img src= {logo} ></img>
         <h1>
           {props.course}
         </h1>
